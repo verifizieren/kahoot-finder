26cdbd626215eb402099cfdf72869ca97a462d43
Max <135263966+verifizieren@users.noreply.github.com>
2023-06-11 20:35:39 +0200

    Update kahoot-finder.py
    refactored the code and made it easier to understand

--- kahoot-finder.py
+++ kahoot-finder.py
@@ -2,46 +2,51 @@ import requests, random, string, concurrent.futures
 from datetime import datetime
 from time import sleep
 
-def gameInfo(id):
-    r = requests.get(f'https://kahoot.it/reserve/session/{id}')
-    return r
+class KahootGameFinder:
+    def __init__(self):
+        self.session = requests.Session()
 
-def generateCode(cleng):
-    code = ''
-    for i in range(cleng):
-        code += random.choice(string.digits)
-    return code
+    def get_game_info(self, id):
+        # Send a GET request to retrieve game information
+        response = self.session.get(f'https://kahoot.it/reserve/session/{id}')
+        return response
 
-def gameFinder():
-    sleep(0.04)
-    code1 = generateCode(6)
-    code2 = generateCode(7)
-    rsc1 = gameInfo(code1).reason
-    rsc2 = gameInfo(code2).reason
-    stc1 = gameInfo(code1).status_code
-    stc2 = gameInfo(code2).status_code
+    def generate_code(self, length):
+        # Generate a random code of specified length
+        code = ''.join(random.choice(string.digits) for _ in range(length))
+        return code
 
-    if rsc1 == "OK":
-        print("Working code found! -> " + code1 + f" Reason -> {rsc1}")
-        time = datetime.now().strftime("%H:%M:%S")
-        f = open("kcodes.txt", "a")
-        f.write(f"{code1} Time -> {time}\n")
-    elif stc1 == 503:
-        sleep(0.5)
-    else:
-        pass
+    def find_game(self):
+        sleep(0.04)
+        code1 = self.generate_code(6)
+        code2 = self.generate_code(7)
+        response1 = self.get_game_info(code1)
+        response2 = self.get_game_info(code2)
 
-    if rsc2 == "OK":
-        print("Working code found! -> " + code2 + f" Reason -> {rsc2}")
-        time = datetime.now().strftime("%H:%M:%S")
-        f = open("kcodes.txt", "a")
-        f.write(f"{code2} Time -> {time}\n")
-    elif stc2 == 503:
-        sleep(0.5)
-    else:
-        pass
+        if response1.reason == "OK":
+            # If the first code is valid, print and store it
+            print("Working code found! -> " + code1 + f" Reason -> {response1.reason}")
+            time = datetime.now().strftime("%H:%M:%S")
+            with open("kcodes.txt", "a") as f:
+                f.write(f"{code1} Time -> {time}\n")
+        elif response1.status_code == 503:
+            # If the first request failed due to server error, wait for a short time
+            sleep(0.5)
 
-threads = int(input("Input the amount of threads: "))
-while True:
-    with concurrent.futures.ThreadPoolExecutor() as executer:
-        proccs = [executer.submit(gameFinder) for _ in range(threads)]
+        if response2.reason == "OK":
+            # If the second code is valid, print and store it
+            print("Working code found! -> " + code2 + f" Reason -> {response2.reason}")
+            time = datetime.now().strftime("%H:%M:%S")
+            with open("kcodes.txt", "a") as f:
+                f.write(f"{code2} Time -> {time}\n")
+        elif response2.status_code == 503:
+            # If the second request failed due to server error, wait for a short time
+            sleep(0.5)
+
+if __name__ == "__main__":
+    threads = int(input("Input the amount of threads: "))
+    game_finder = KahootGameFinder()
+    while True:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            # Start multiple threads to perform game finding concurrently
+            processes = [executor.submit(game_finder.find_game) for _ in range(threads)]
